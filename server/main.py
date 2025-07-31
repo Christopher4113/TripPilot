3fb88350d51acc45ad3fb9625b43029431dc37dc
Christopher4113 <ch.lam1328@gmail.com>
2025-07-31 15:55:45 -0400

    Added JWT extraction

--- server/main.py
+++ server/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from helpers.extractToken import get_current_user
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI"}
+
+
+@app.get("/me")
+def get_me(current_user: dict = Depends(get_current_user)):
+    return {
+        "user_id": current_user["user_id"],
+        "username": current_user["username"]
+    }
